eab28453c05caff9b51a50e385cfc055f991350a
kamoliddinfrontdev <kamoliddinfrontdev@gmail.com>
2025-04-18 17:09:01 +0500

    carousel added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import TheNavbar from './components/TheNavbar.vue'
-
+import TheCarousel from './components/TheCarousel.vue';
 </script>
 
 <template>
   <TheNavbar />
+  <TheCarousel/>
    <main class="bg-Cream w-[100%] h-[100vh] "></main>
 
   
---
+++ src/components/TheCarousel.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="(item, index) in items" :key="index">
+      <div class="carousel-content">
+        {{ item }}
+      </div>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        "Fayl1.txt",
+        "Fayl2.pdf",
+        "Fayl3.docx",
+      ], // Fayl nomlarini shu yerga qo'shing
+    };
+  },
+};
+</script>
+
+<style scoped>
+.carousel-content {
+  font-size: 20px;
+  text-align: center;
+  padding: 20px;
+  color: #333;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
